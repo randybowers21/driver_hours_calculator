610aba9c58c1a3954ab7d31c44b6be58f1ccf08c
Randy Bowers <85922903+randybowers21@users.noreply.github.com>
2023-04-07 09:04:00 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+#This is here to help with some imports I dont really understand it right now
 from setuptools import setup, find_packages
 
 setup(name='myproject', version='1.0', packages=find_packages())
